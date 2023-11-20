cac7666f93289446e5a272fecce91cbf481e87e2
Henrique Oliveira <contato.animous@gmail.com>
2023-11-20 16:09:23 -0300

    Aprimoramento no gerador e processamento

--- gerador.py
+++ gerador.py
@@ -1,20 +1,52 @@
 from faker import Faker
 import csv
+from hdfs import InsecureClient
 
+# Inicializa a biblioteca Faker para criar dados falsos.
 fake = Faker()
 
+# Define o número de clientes e inicia uma lista vazia para armazenar os dados gerados.
 num_clientes = 1000
-client_data = []
+teste = []
+
+# Para cada cliente, gera dados falsos de nome, endereço, email e número de telefone, utilizando o Faker.
 for _ in range(num_clientes):
-    client_data.append({
-        'nome': fake.name(),
-        'endereco': fake.address(),
-        'email': fake.email(),
-        'numero_telefone': fake.phone_number(),
+    nome = fake.name()
+    endereco = fake.address()
+    email = fake.email()
+    numero_telefone = fake.phone_number()
+
+    # Adiciona um dicionário contendo os dados do cliente à lista.
+    teste.append({
+        'nome': nome,
+        'endereco': endereco,
+        'email': email,
+        'numero_telefone': numero_telefone,     
     })
 
-with open('client_data.csv', 'w', newline='') as file:
+# Define o caminho local onde o arquivo CSV será criado.
+local_file_path = './exemplos_csv/teste.csv'
+
+# Abre o arquivo CSV no modo de escrita, cria um escritor CSV e escreve os dados da lista no arquivo CSV.
+with open(local_file_path, 'w', newline='') as file:
     fieldnames = ['nome', 'endereco', 'email', 'numero_telefone']
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     writer.writeheader()
-    writer.writerows(client_data)
+    writer.writerows(teste)
+
+# Cria um Client para interagir com o Hadoop utilizando o InsecureClient.
+client = InsecureClient('http://localhost:9870', user='animo', root="/webhdfs/v1")
+
+# Define o caminho e o nome do arquivo no Hadoop.
+hadoop_directory = '/cay'
+file_name = 'teste.csv'
+hadoop_path = f"{hadoop_directory}/{file_name}"
+
+# Abre o arquivo CSV local no modo de leitura binária e utiliza o Client para escrever esse arquivo no Hadoop.
+with open(local_file_path, 'rb') as hdfs_file:
+    client.write(hadoop_path, hdfs_file, overwrite=True)
+
+# Exibe uma mensagem informando que o arquivo foi enviado com sucesso para o Hadoop.
+print(f"Arquivo {file_name} enviado para {hadoop_directory} no HDFS com sucesso!")
+
+
--- processamento.py
+++ processamento.py
@@ -1,11 +1,29 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import when
+import os
 
+# Inicialização de uma sessão Spark.
 spark = SparkSession.builder \
     .appName("Lendo arquivo CSV do Hadoop") \
     .master("local") \
     .getOrCreate()
 
-file_path = "hdfs://localhost:9000/cay/client_data.csv"
+# Definição do caminho do arquivo CSV no hadoop.
+file_path = "hdfs://localhost:9000/cay/teste.csv"
+
+# Leitura do arquivo CSV localizado no hadoop para criar um DataFrame Spark.
 df = spark.read.csv(file_path, header=True, inferSchema=True)
 
-df.show(5)
+# Preenche valores nulos nas colunas 'email' e 'numero_telefone' do DataFrame com valores padrão.
+df = df.fillna({'email': 'email@example.com', 'numero_telefone': '000-000-0000'})
+
+# Comandos para excluir um diretório no Hadoop
+'''
+diretorio_a_remover = "hdfs://localhost:9000/cay/teste_tratado.csv"
+
+comando_remover = f"hdfs dfs -rm -r -skipTrash {diretorio_a_remover}"
+
+os.system(comando_remover)
+'''
+
+
